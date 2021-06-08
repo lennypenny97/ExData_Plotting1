6424bd69861ff9dbcd7807154234bed855e36c2c
lennypenny97 <83807781+lennypenny97@users.noreply.github.com>
2021-06-08 13:08:32 +0200

    Add files via upload

---
+++ Assignment_Week1.R
@@ -0,0 +1,64 @@
+# Exploratory Data Analysis
+
+# Assignment - Week 1 - lennypenny97
+
+## Loading and subsetting the data 
+
+temp <- tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+power <- read.table(unz(temp,"household_power_consumption.txt"), 
+                    sep=";", 
+                    header = T, 
+                    na="?", 
+                    colClasses = c("character",
+                                   'character',
+                                   'numeric',
+                                   'numeric',
+                                   'numeric',
+                                   'numeric',
+                                   'numeric',
+                                   'numeric',
+                                   'numeric'))
+
+unlink(temp)
+power <- power[which(power$Date == '2/2/2007' | power$Date=='1/2/2007'),]
+
+power$POSIX <-as.POSIXlt.character(paste(power$Date,power$Time),format = "%d/%m/%Y %H:%M:%S")
+
+
+# Plot 1 - Global active power Histogram
+
+png(filename="plot1.png",width=480, height=480)
+hist(power$Global_active_power, col = 'red', main = 'Global Active Power', xlab = 'Global Active Power (kilowatts)')
+dev.off()
+
+
+# Plot 2 - Global active power - Frequency per days
+
+png(filename="plot2.png",width=480, height=480)
+plot(x=power$POSIX ,y=power$Global_active_power, type = 'l', xlab='',ylab = 'Global Active Power (kilowatts)')
+dev.off()
+
+
+# Plot 3 - Energy sub metering 
+
+png(filename="plot3.png",width=480, height=480)
+plot(x=power$POSIX,y=power$Sub_metering_1, type='l', col = 'black', ylab = 'Energy sub metering', xlab = '')
+lines(x=power$POSIX,y=power$Sub_metering_2, col='red')
+lines(x=power$POSIX,y=power$Sub_metering_3, col='blue')
+legend('topright', legend = c('Sub_metering_1',"Sub_metering_2","Sub_metering_3"), col = c('black','red','blue'), lty = 1)
+dev.off()
+
+
+# Plot 4 - Global active power/Voltage/Energy sub metering/Global reactive power 4x4 plot
+
+png(filename="plot4.png",width=480, height=480)
+par(mfrow=c(2,2))
+plot(x=power$POSIX ,y=power$Global_active_power, type = 'l', xlab='',ylab = 'Global Active Power')
+plot(x=power$POSIX ,y=power$Voltage, type = 'l', xlab='datetime',ylab = 'Voltage')
+plot(x=power$POSIX,y=power$Sub_metering_1, type='l', col = 'black', ylab = 'Energy sub metering', xlab = '')
+lines(x=power$POSIX,y=power$Sub_metering_2, col='red')
+lines(x=power$POSIX,y=power$Sub_metering_3, col='blue')
+legend('topright', legend = c('Sub_metering_1',"Sub_metering_2","Sub_metering_3"), col = c('black','red','blue'), lty = 1, bty = "n")
+plot(x=power$POSIX ,y=power$Global_reactive_power, type = 'l', xlab='datetime',ylab = 'Global_reactive_power')
+dev.off()
